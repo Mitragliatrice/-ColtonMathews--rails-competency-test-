e67ea635d839d44beff400d156a42222c287d96a
Colton Mathews <mathews.colton@gmail.com>
2017-09-12 12:33:30 -0600

    created seed file for test data and put top three of the 3 categories on home page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  User.create!(email: "admin@test.com", password: "asdfasdf", roles: "admin")
+  p "Admin created"
+  User.create!(email: "editor@test.com", password: "asdfasdf", roles: "editor")
+  p "Editor Created"
+  User.create!(email: "user@test.com", password: "asdfasdf", roles: "user")
+  p "User Created"
+
+
+5.times do |art|
+  Article.create!(title: "stuff #{art}", content: "Ipsum labore do duis quis voluptate ex consequat sint irure tempor do ex. Reprehenderit ut minim excepteur veniam aute officia consequat Lorem quis duis adipisicing est ea adipisicing. Aliqua culpa magna minim consequat cupidatat exercitation cupidatat adipisicing velit laboris magna excepteur. Amet et cupidatat commodo veniam qui occaecat. Enim sint tempor adipisicing exercitation aliqua excepteur nisi minim exercitation anim. Aute et dolor labore laboris.", category: "stuff", user_id: User.last )
+end
+
+p "5 stuff articles created by Vanilla User"
+
+5.times do |art|
+  Article.create!(title: "things #{art}", content: "Ipsum labore do duis quis voluptate ex consequat sint irure tempor do ex. Reprehenderit ut minim excepteur veniam aute officia consequat Lorem quis duis adipisicing est ea adipisicing. Aliqua culpa magna minim consequat cupidatat exercitation cupidatat adipisicing velit laboris magna excepteur. Amet et cupidatat commodo veniam qui occaecat. Enim sint tempor adipisicing exercitation aliqua excepteur nisi minim exercitation anim. Aute et dolor labore laboris.", category: "stuff", user_id: User.last )
+end
+
+p "5 things articles created by Vanilla User"
+
+5.times do |art|
+  Article.create!(title: "those #{art}", content: "Ipsum labore do duis quis voluptate ex consequat sint irure tempor do ex. Reprehenderit ut minim excepteur veniam aute officia consequat Lorem quis duis adipisicing est ea adipisicing. Aliqua culpa magna minim consequat cupidatat exercitation cupidatat adipisicing velit laboris magna excepteur. Amet et cupidatat commodo veniam qui occaecat. Enim sint tempor adipisicing exercitation aliqua excepteur nisi minim exercitation anim. Aute et dolor labore laboris.", category: "stuff", user_id: User.last )
+end
+
+p "5 those articles created by Vanilla User"
--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  describe 'index' do
+  describe 'everyone can access index' do
     it 'can be reached successfully' do
       visit articles_path
       expect(page.status_code).to eq(200)
