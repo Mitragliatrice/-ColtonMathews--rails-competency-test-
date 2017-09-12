c3a20167d838e2a5d1b507a9a6398f21274c4ab1
Colton Mathews <mathews.colton@gmail.com>
2017-09-12 12:03:33 -0600

    passed test to reach index page located at root.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,41 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :destroy, :edit]
+
+def index
+end
+
+def show
+end
+
+def new
+end
+
+def create
+end
+
+def show
+end
+
+def edit
+end
+
+def destroy
+end
+
+private
+
+def fetch_params
+  @article = Article.find(params[:id])
+end
+
+def set_article
+  params.require(:article).permit(:title,
+                                      :content,
+                                      :category,
+                                      :user_id
+                                      )
+end
+
+
+
 end
--- db/migrate/20170912175201_create_articles.rb
+++ db/migrate/20170912175201_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.test :content
+      t.text :content
       t.string :category
       t.integer :user_id
 
